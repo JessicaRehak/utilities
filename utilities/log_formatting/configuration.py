608f35c9c339839d06daebec6d3ee6b017d38ff6
Jessica Rehak <rehak@lbl.gov>
2025-01-14 10:32:32 -0800

    initial commit of logging configuration and verbose formatter

---
+++ utilities/log_formatting/configuration.py
@@ -0,0 +1,59 @@
+from logging.config import dictConfig
+
+LOGGING_CONFIG = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(message)s',
+        },
+        'verbose': {
+            '()': 'utilities.log_formatting.verbose_formatter.VerboseFormatter'
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'formatter': 'standard',
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://sys.stdout',
+        },
+        'debug_console': {
+            'level': 'DEBUG',
+            'formatter': 'verbose',
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://sys.stdout',
+        },
+        'rotating_file_handler': {
+            'level': 'INFO',
+            'formatter': 'verbose',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': 'info.log',
+            'mode': 'a',
+            'maxBytes': 1048576,
+            'backupCount': 100,
+        },
+        'file_handler': {
+            'level': 'WARNING',
+            'formatter': 'verbose',
+            'class': 'logging.FileHandler',
+            'filename': 'error.log',
+            'mode': 'a',
+        }
+
+    },
+}
+
+def configure_logging(caller_name: str = '',
+                      debug_console: bool = False,
+                      log_to_file: bool = False) -> None:
+    logger_configuration = {caller_name: {'propagate': False, 'level': 'INFO'}}
+    
+    if debug_console:
+        logger_configuration[caller_name]['handlers'] = ['debug_console']
+    else:
+        logger_configuration[caller_name]['handlers'] = ['console']
+    if log_to_file:
+        logger_configuration[caller_name]['handlers'].extend(['rotating_file_handler', 'file_handler'])
+    LOGGING_CONFIG['loggers'] = logger_configuration
+    dictConfig(LOGGING_CONFIG)
---
+++ utilities/log_formatting/verbose_formatter.py
@@ -0,0 +1,22 @@
+import logging
+from typing import Dict
+
+from colorama import Fore, Style
+
+class VerboseFormatter(logging.Formatter):
+    _issue_format: str = '%(asctime)s {name} {{color}}{level:^10} {reset}%(message)s'.format( name='%(name)-10s', level='%(levelname)-7s', reset=Style.RESET_ALL,)
+    _formats: Dict[int, str] = {logging.ERROR: _issue_format.format(color=Fore.RED), 
+                                logging.WARNING: _issue_format.format(color=Fore.YELLOW), 
+                                logging.DEBUG: _issue_format.format(color=Fore.CYAN), 
+                                logging.INFO: _issue_format.format(color=Fore.WHITE), 
+                                }
+    default_time_format = '%H:%M:%S'
+    default_msec_format = '%s.%03d'
+   
+    def __init__(self):
+        super().__init__()
+        
+    def format(self, record) -> str:
+        self._style._fmt = VerboseFormatter._formats[record.levelno]
+        return logging.Formatter.format(self, record)
+    
