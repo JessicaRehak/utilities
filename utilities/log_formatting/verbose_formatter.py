3a474dd2734c99fb5185e1c16c7b2cfc9c8b35c5
Jessica Rehak <rehak@lbl.gov>
2025-01-14 12:56:27 -0800

    refactored internals of VerboseConsoleFormatter, added VerboseFormatter

--- utilities/log_formatting/verbose_formatter.py
+++ utilities/log_formatting/verbose_formatter.py
@@ -4,19 +4,26 @@ from typing import Dict
 from colorama import Fore, Style
 
 class VerboseConsoleFormatter(logging.Formatter):
-    _issue_format: str = '%(asctime)s {name} {{color}}{level:^10} {reset}%(message)s'.format( name='%(name)-10s', level='%(levelname)-7s', reset=Style.RESET_ALL,)
-    _formats: Dict[int, str] = {logging.ERROR: _issue_format.format(color=Fore.RED), 
-                                logging.WARNING: _issue_format.format(color=Fore.YELLOW), 
-                                logging.DEBUG: _issue_format.format(color=Fore.CYAN), 
-                                logging.INFO: _issue_format.format(color=Fore.WHITE), 
-                                }
+    """A verbose formatter that includes ASCII escape characters for coloring."""
+    _issue_format: str = '%(asctime)s {name} {{color}}{level:^10} {reset}%(message)s'.format( name='%(name)-20s', level='%(levelname)-10s', reset=Style.RESET_ALL,)
     default_time_format = '%H:%M:%S'
     default_msec_format = '%s.%03d'
+
+    def _get_color(self, logging_level: int) -> str:
+        color_dictionary = {logging.ERROR: Fore.RED, 
+                logging.WARNING: Fore.YELLOW, 
+                logging.DEBUG: Fore.CYAN, 
+                logging.INFO: Fore.WHITE}
+        return color_dictionary[logging_level]
    
     def __init__(self):
         super().__init__()
         
     def format(self, record) -> str:
-        self._style._fmt = VerboseConsoleFormatter._formats[record.levelno]
+        self._style._fmt = self._issue_format.format(color=self._get_color(record.levelno))
         return logging.Formatter.format(self, record)
     
+
+class VerboseFormatter(VerboseConsoleFormatter):
+    def _get_color(logging_level):
+        return Fore.WHITE
