f6fe86e8e51aad74a2a43dd59fbb2bab80ffb3f7
Jessica Rehak <rehak@lbl.gov>
2025-04-10 15:13:38 -0700

    overhauled the run_command command

--- utilities/subprocess/run_command.py
+++ utilities/subprocess/run_command.py
@@ -17,7 +17,8 @@ def run_pool(args: List[List[str]], cwd: Path) -> str:
 def run_command(args: List[str | Any], cwd: Path, *, check_return_code: bool = True, 
                 print_output_to_debug: bool = False,
                 run_in_shell: bool = False,
-                input_file: Optional[Path] = None) -> str:
+                input_file: Optional[Path] = None,
+                capture_output: bool = False) -> int | tuple[int, bytes]:
     """Runs a subprocess command. With the arguments provided in a list of strings or
     objects that can be converted to strings. Optionally checks the return code to raise
     an error. 
@@ -37,25 +38,33 @@ def run_command(args: List[str | Any], cwd: Path, *, check_return_code: bool = T
     _log.debug('Running command {} at {}'.format(' '.join(args), cwd))
     if input_file is not None:
         _log.debug('With input file: {}'.format(input_file))
-        infile = open(input_file)
+        infile = open(input_file, 'r')
     else:
         infile = None
-    with subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd,
-                        text=True, encoding='utf-8', shell=run_in_shell, 
-                        stdin=infile) as proc:
-        for line in proc.stdout:
-            if print_output_to_debug:
-                _log.debug(line.rstrip('\n'))
-        return_code = proc.wait()
-    _log.debug('Process finished with code {}'.format(return_code))
-    if input_file is not None:
-        infile.close()
-    if check_return_code and return_code != 0:
-        try:
-            error = proc.stderr.decode('utf-8')
-        except UnicodeDecodeError:
-            # Try decoding, if it can't be decoded, just send the raw output byte value as a fallback
-            error = proc.stderr
-        _log.error("Command error running {}: {}".format(args[0], error))
-        raise subprocess.SubprocessError
-    return return_code
+    try:
+        process = subprocess.Popen(args, 
+                                   stdout=subprocess.PIPE if print_output_to_debug or capture_output else None, 
+                                   stderr=subprocess.PIPE, 
+                                   cwd=cwd, 
+                                   text=False, 
+                                   shell=run_in_shell, stdin=infile)
+
+        stdout, stderr = process.communicate()
+        if print_output_to_debug and stdout is not None:
+            for line in stdout.decode('utf-8', errors="replace").splitlines():
+                _log.debug(line)
+        return_code = process.returncode
+        _log.debug('Process finished with code {}'.format(return_code))
+
+        if check_return_code and return_code != 0:
+            error_msg = stderr.decode("utf-8", errors="replace") if stderr else "<no error output>"
+            _log.error("Command error running {}: {}".format(args[0], error_msg))
+            raise subprocess.SubprocessError
+        
+        if capture_output:
+            return return_code, stdout
+        return return_code
+    finally:
+        if infile is not None:
+            infile.close()
+        
